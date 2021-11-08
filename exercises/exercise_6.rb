408a3c44ffc2803e9b9c8d8b0c60cc2ef8489579
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-11-08 15:29:39 +0000

    relation between stores and employees created in STORE and EMPLOYEE class, employees added in db

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,47 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(
+  first_name:'abc',
+  last_name:'cef',
+  hourly_rate: 50
+)
+
+@store1.employees.create(
+  first_name:'ghi',
+  last_name:'jkl',
+  hourly_rate: 40
+)
+
+@store1.employees.create(
+  first_name:'mno',
+  last_name:'pqr',
+  hourly_rate: 55
+)
+
+@store2.employees.create(
+  first_name:'stu',
+  last_name:'vwz',
+  hourly_rate: 85
+)
+
+@store2.employees.create(
+  first_name:'xyz',
+  last_name:'abc',
+  hourly_rate: 25
+)
+
+@store2.employees.create(
+  first_name:'ddyz',
+  last_name:'abdfdc',
+  hourly_rate: 40
+)
+
+@store2.employees.create(
+  first_name:'asyz',
+  last_name:'sadf',
+  hourly_rate: 50
+)
+
+
