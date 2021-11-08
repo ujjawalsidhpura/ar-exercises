18281ce383f509d8c2ed9d270f65515d4d0c343d
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-11-08 15:21:20 +0000

    ex 5: quering with WHERE

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue =  Store.sum(:annual_revenue)
+@total_stores = Store.count(:name)
+
+puts "Total revenue of all stores across canada is $#{@total_revenue}"
+
+puts "Average annual income of all stores is #{@total_revenue/@total_stores}"
+
+@store_over_million = Store.where(annual_revenue: 1000000..Float::INFINITY).count
+
+puts @store_over_million
