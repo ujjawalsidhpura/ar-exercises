cc4ce4ec6122e0fe36a5fc8c84ad17fc232dfa2a
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-11-08 13:51:56 +0000

    exercise one, 3 store entries created

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,30 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# Burnaby = Store.new
+# Richmond = Store.new
+# Gastown = Store.new
+
+Burnaby = Store.create(
+  name: 'Burnaby',
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+Richmond = Store.create(
+  name: 'Richmond',
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+Gastown = Store.create(
+  name: 'Gastown',
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+puts Store.count
